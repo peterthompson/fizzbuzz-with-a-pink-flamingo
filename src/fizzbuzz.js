dcbc20b470e5b73a72580509236776e8cb160ca1
Pete Thompson <me@petethompson.com>
2019-02-23 09:20:09 +0000

    Add fizzbuzz implementation

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,19 @@
 function fizzbuzz(number) {
-  return;
+  let result;
+
+  if (number === 0) {
+    result = 0;
+  } else if (number % 15 === 0) {
+    result = "FizzBuzz";
+  } else if (number % 5 === 0) {
+    result = "Buzz";
+  } else if (number % 3 === 0) {
+    result = "Fizz";
+  } else {
+    result = number;
+  }
+
+  return result;
 }
 
 export default fizzbuzz;
