056108202cc10b5c8cffe482983ff9488dfedce8
Pete Thompson <me@petethompson.com>
2019-02-23 12:46:57 +0000

    Add isFibonacci implementation

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
+function isPerfectSquare(number) {
+  return number % Math.sqrt(number) === 0;
+}
+
 function isFibonacci(number) {
-  return;
+  return (
+    isPerfectSquare(5 * number * number + 4) ||
+    isPerfectSquare(5 * number * number - 4)
+  );
 }
 
 export default isFibonacci;
